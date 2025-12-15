62e503d0112565eafd8a090479081894a91f9b99
saraivajv <joaovictors2010@gmail.com>
2025-12-15 00:40:51 -0300

    choreography e orchestration finalizados

--- src/main/java/com/imd/payroll_service/config/OrchestrationConfig.java
+++ src/main/java/com/imd/payroll_service/config/OrchestrationConfig.java
@@ -16,7 +16,7 @@ public class OrchestrationConfig {
             System.out.println("[Orquestração] Recebido comando para validar: " + cmd.name());
             boolean approved = cmd.salary() <= 50000;
 
-            String reason = approved ? "OK" : "Salary exceeds limit of 50,000";
+            String reason = approved ? "OK" : "Salário muito alto";
             System.out.println("-> Resultado: " + (approved ? "APROVADO" : "REJEITADO"));
 
             return new SalaryValidationResult(
